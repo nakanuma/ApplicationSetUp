3f5369b3c17e13ea388471196efcddcfceb9f587
nakanuma <k023g0121@g.neec.ac.jp>
2025-04-23 16:48:30 +0900

    途中

--- Application/Application/TestScene.cpp
+++ Application/Application/TestScene.cpp
@@ -30,24 +30,108 @@ void TestScene::Initialize() {
 	lightManager = LightManager::GetInstance();
 	lightManager->Initialize();
 
-	// レンダーテクスチャ生成
-	renderTexture_ = RTVManager::CreateRenderTargetTexture(Window::GetWidth(), Window::GetHeight());
-
 	///
 	///	↓ ゲームシーン用
 	///
 
 	// Texture読み込み
-	uint32_t uvCheckerGH = TextureManager::Load("resources/Images/uvChecker.png", dxBase->GetDevice());
+	uint32_t uvCheckerGH = TextureManager::Load("resources/Images/white.png", dxBase->GetDevice());
 
 	// モデルの読み込みとテクスチャの設定
-	model_ = ModelManager::LoadModelFile("resources/Models", "plane.obj", dxBase->GetDevice());
+	model_ = ModelManager::LoadModelFile("resources/Models", "cube.obj", dxBase->GetDevice());
 	model_.material.textureHandle = uvCheckerGH;
 
 	// オブジェクトの生成とモデル設定
 	object_ = std::make_unique<Object3D>();
 	object_->model_ = &model_;
 	object_->transform_.rotate = {0.0f, 3.14f, 0.0f};
+
+	///
+	///	レンダーテクスチャ関連
+	/// 
+
+	// レンダーテクスチャ生成
+	renderTextureGH_ = RTVManager::CreateRenderTargetTexture(Window::GetWidth(), Window::GetHeight());
+	outlineGH_ = RTVManager::CreateRenderTargetTexture(Window::GetWidth(), Window::GetHeight(), {0.0f, 0.0f, 0.0f, 0.0f});
+
+
+	// Sprite用のリソースを作る
+	vertexResourceSprite = CreateBufferResource(dxBase->GetDevice(), sizeof(Sprite::VertexData) * 4);
+	// 頂点バッファビューを作成する
+	vertexBufferViewSprite = {};
+	// リソースの先頭のアドレスから使う
+	vertexBufferViewSprite.BufferLocation = vertexResourceSprite->GetGPUVirtualAddress();
+	// 使用するリソースのサイズは頂点4つ分のサイズ
+	vertexBufferViewSprite.SizeInBytes = sizeof(Sprite::VertexData) * 4;
+	// 1頂点あたりのサイズ
+	vertexBufferViewSprite.StrideInBytes = sizeof(Sprite::VertexData);
+
+	// 頂点データを設定
+	vertexDataSprite = nullptr;
+	vertexResourceSprite->Map(0, nullptr, reinterpret_cast<void**>(&vertexDataSprite));
+	// 左下
+	vertexDataSprite[0].position = {0.0f, 360.0f, 0.0f, 1.0f};
+	vertexDataSprite[0].texcoord = {0.0f, 1.0f};
+	vertexDataSprite[0].normal = {0.0f, 0.0f, -1.0f};
+	// 左上
+	vertexDataSprite[1].position = {0.0f, 0.0f, 0.0f, 1.0f};
+	vertexDataSprite[1].texcoord = {0.0f, 0.0f};
+	vertexDataSprite[1].normal = {0.0f, 0.0f, -1.0f};
+	// 右下
+	vertexDataSprite[2].position = {640.0f, 360.0f, 0.0f, 1.0f};
+	vertexDataSprite[2].texcoord = {1.0f, 1.0f};
+	vertexDataSprite[2].normal = {0.0f, 0.0f, -1.0f};
+	// 右上
+	vertexDataSprite[3].position = {640.0f, 0.0f, 0.0f, 1.0f};
+	vertexDataSprite[3].texcoord = {1.0f, 0.0f};
+	vertexDataSprite[3].normal = {0.0f, 0.0f, -1.0f};
+
+
+	// 頂点インデックスの作成
+	indexResourceSprite = CreateBufferResource(dxBase->GetDevice(), sizeof(uint32_t) * 6);
+	// インデックスバッファビューの作成
+	indexBufferViewSprite = {};
+	// リソースの先頭のアドレスから使う
+	indexBufferViewSprite.BufferLocation = indexResourceSprite->GetGPUVirtualAddress();
+	// 使用するリソースのサイズはインデックス6つ分のサイズ
+	indexBufferViewSprite.SizeInBytes = sizeof(uint32_t) * 6;
+	// インデックスはuin32_tとする
+	indexBufferViewSprite.Format = DXGI_FORMAT_R32_UINT;
+
+	// インデックスリソースにデータを書き込む
+	indexDataSprite = nullptr;
+	indexResourceSprite->Map(0, nullptr, reinterpret_cast<void**>(&indexDataSprite));
+	indexDataSprite[0] = 0; indexDataSprite[1] = 1; indexDataSprite[2] = 2; 
+	indexDataSprite[3] = 1; indexDataSprite[4] = 3; indexDataSprite[5] = 2;
+
+	// Outline用のマテリアルのリソースを作る
+	outlineMaterial.data_->color = {0.0f, 0.0f, 0.0f, 1.0f};
+	outlineMaterial.data_->enableLighting = false;
+	outlineMaterial.data_->uvTransform = Matrix::Identity();
+
+
+	// Sprite用のTransformationMatrix用のリソースを作る。Matrix 1つ分のサイズを用意する
+	transformationMatrixResourceSprite = CreateBufferResource(dxBase->GetDevice(), sizeof(Object3D::TransformationMatrix));
+	// データを書き込む
+	transformationMatrixDataSprite = nullptr;
+	// 書き込むためのアドレスを取得
+	transformationMatrixResourceSprite->Map(0, nullptr, reinterpret_cast<void**>(&transformationMatrixDataSprite));
+	// 単位行列を書き込んでおく
+	transformationMatrixDataSprite->WVP = Matrix::Identity();
+
+
+	// Sprite用のマテリアルのリソースを作る
+	materialResourceSprite = CreateBufferResource(dxBase->GetDevice(), sizeof(Object3D::Material));
+	// マテリアルにデータを書き込む
+	materialDataSprite = nullptr;
+	// 書き込むためのアドレスを取得
+	materialResourceSprite->Map(0, nullptr, reinterpret_cast<void**>(&materialDataSprite));
+	// 輝度を白に設定
+	materialDataSprite->color = Float4(1.0f, 1.0f, 1.0f, 1.0f);
+	// SpriteはLightingしないのでfalseを設定
+	materialDataSprite->enableLighting = false;
+	// UVTransform行列を単位行列で初期化
+	materialDataSprite->uvTransform = Matrix::Identity();
 }
 
 void TestScene::Finalize() {}
@@ -72,16 +156,79 @@ void TestScene::Draw() {
 	// ライトの定数バッファを設定
 	lightManager->TransferContantBuffer();
 
-	// レンダーターゲットをレンダーテクスチャにセット
-	RTVManager::SetRenderTarget(renderTexture_);
-	RTVManager::ClearRTV(renderTexture_);
+	#ifdef _DEBUG
+	ImGuiUtil::ImageWindow("rendertexture", renderTextureGH_);
+	ImGuiUtil::ImageWindow("outline", outlineGH_);
+	#endif
 
 	///
 	///	↓ ここから3Dオブジェクトの描画コマンド
 	///
 
+	// レンダーターゲットをレンダーテクスチャにセット
+	RTVManager::SetRenderTarget(renderTextureGH_);
+	RTVManager::ClearRTV(renderTextureGH_);
+
 	// オブジェクトの描画
 	object_->Draw();
+
+	#pragma region 深度値を元にアウトライン生成
+	// レンダーターゲットの設定
+	RTVManager::SetRenderTarget(outlineGH_);
+	RTVManager::ClearRTV(outlineGH_, {0.0f, 0.0f, 0.0f, 0.0f});
+	// ポストエフェクト用のPSOを設定
+	dxBase->GetCommandList()->SetPipelineState(dxBase->GetPipelineStateSobelFilter());
+	// VBVを設定
+	dxBase->GetCommandList()->IASetVertexBuffers(0, 1, &vertexBufferViewSprite);
+	// IBVを設定
+	dxBase->GetCommandList()->IASetIndexBuffer(&indexBufferViewSprite);
+	// マテリアルCBufferの場所を設定
+	dxBase->GetCommandList()->SetGraphicsRootConstantBufferView(0, outlineMaterial.resource_->GetGPUVirtualAddress());
+	// TransformationMatrixCBufferの場所を設定
+	dxBase->GetCommandList()->SetGraphicsRootConstantBufferView(1, transformationMatrixResourceSprite->GetGPUVirtualAddress());
+	// SRVのDescriptorTableの先頭を設定
+	TextureManager::SetDescriptorTable(2, dxBase->GetCommandList(), RTVManager::GetDepthSRVHandle(renderTextureGH_));
+	// 描画。6個のインデックスを使用し1つのインスタンスを描画
+	dxBase->GetCommandList()->DrawIndexedInstanced(6, 1, 0, 0, 0);
+	#pragma endregion
+
+
+	#pragma region renderTextureGHをバックバッファにそのまま描画
+	// レンダーターゲットの設定
+	RTVManager::SetRTtoBB();
+	// 通常のパイプラインを設定
+	dxBase->GetCommandList()->SetPipelineState(dxBase->GetPipelineState());
+	// VBVを設定
+	dxBase->GetCommandList()->IASetVertexBuffers(0, 1, &vertexBufferViewSprite);
+	// IBVを設定
+	dxBase->GetCommandList()->IASetIndexBuffer(&indexBufferViewSprite);
+	// マテリアルCBufferの場所を設定
+	dxBase->GetCommandList()->SetGraphicsRootConstantBufferView(0, materialResourceSprite->GetGPUVirtualAddress());
+	// TransformationMatrixCBufferの場所を設定
+	dxBase->GetCommandList()->SetGraphicsRootConstantBufferView(1, transformationMatrixResourceSprite->GetGPUVirtualAddress());
+	// SRVのDescriptorTableの先頭を設定
+	TextureManager::SetDescriptorTable(2, dxBase->GetCommandList(), renderTextureGH_);
+	// 描画。6個のインデックスを使用し1つのインスタンスを描画
+	dxBase->GetCommandList()->DrawIndexedInstanced(6, 1, 0, 0, 0);
+	#pragma endregion
+
+
+	#pragma region outlineをバックバッファにそのまま描画
+	// 通常のパイプラインを設定
+	dxBase->GetCommandList()->SetPipelineState(dxBase->GetPipelineState());
+	// VBVを設定
+	dxBase->GetCommandList()->IASetVertexBuffers(0, 1, &vertexBufferViewSprite);
+	// IBVを設定
+	dxBase->GetCommandList()->IASetIndexBuffer(&indexBufferViewSprite);
+	// マテリアルCBufferの場所を設定
+	dxBase->GetCommandList()->SetGraphicsRootConstantBufferView(0, materialResourceSprite->GetGPUVirtualAddress());
+	// TransformationMatrixCBufferの場所を設定
+	dxBase->GetCommandList()->SetGraphicsRootConstantBufferView(1, transformationMatrixResourceSprite->GetGPUVirtualAddress());
+	// SRVのDescriptorTableの先頭を設定
+	TextureManager::SetDescriptorTable(2, dxBase->GetCommandList(), outlineGH_);
+	// 描画。6個のインデックスを使用し1つのインスタンスを描画
+	dxBase->GetCommandList()->DrawIndexedInstanced(6, 1, 0, 0, 0);
+	#pragma endregion
 
 	///
 	///	↑ ここまで3Dオブジェクトの描画コマンド
@@ -99,20 +246,13 @@ void TestScene::Draw() {
 	///
 
 #ifdef _DEBUG
-
-#endif // _DEBUG
-
 	ImGui::Begin("window");
 
 	ImGui::DragFloat3("translation", &object_->transform_.translate.x, 0.01f);
 	ImGui::DragFloat3("rotation", &object_->transform_.rotate.x, 0.01f);
 
 	ImGui::End();
-
-	// レンダーテクスチャをImGuiWindowに描画
-	ImGuiUtil::ImageWindow("Scene", renderTexture_);
-	// レンダーテクスチャをバックバッファに描画
-	RTVManager::SetRTtoBB();
+#endif // _DEBUG
 
 	// ImGuiの内部コマンドを生成する
 	ImguiWrapper::Render(dxBase->GetCommandList());
--- Application/Application/TestScene.h
+++ Application/Application/TestScene.h
@@ -36,6 +36,34 @@ private:
 	// 3Dオブジェクト
 	std::unique_ptr<Object3D> object_;
 
+	///
+	/// レンダーテクスチャ関連
+	/// 
+
 	// レンダーテクスチャ
-	uint32_t renderTexture_;
+	uint32_t renderTextureGH_;
+	uint32_t outlineGH_;
+
+	// スプライト用のリソース
+	Microsoft::WRL::ComPtr<ID3D12Resource> vertexResourceSprite;
+	// 頂点データ
+	Sprite::VertexData* vertexDataSprite;
+	// 頂点バッファビュー
+	D3D12_VERTEX_BUFFER_VIEW vertexBufferViewSprite{};
+	// インデックスデータ
+	uint32_t* indexDataSprite = nullptr;
+	// 頂点インデックス
+	Microsoft::WRL::ComPtr<ID3D12Resource> indexResourceSprite;
+	// インデックスバッファビュー
+	D3D12_INDEX_BUFFER_VIEW indexBufferViewSprite{};
+	// アウトライン用のマテリアルリソース
+	ConstBuffer<Sprite::Material> outlineMaterial;
+	// Sprite用のTransformationMatrix用のリソース
+	Microsoft::WRL::ComPtr<ID3D12Resource> transformationMatrixResourceSprite;
+	// Sprite用のマテリアルのリソース
+	Microsoft::WRL::ComPtr<ID3D12Resource> materialResourceSprite;
+	// トランスフォーム用データ
+	Object3D::TransformationMatrix* transformationMatrixDataSprite;
+	// マテリアル用データ
+	Object3D::Material* materialDataSprite;
 };
