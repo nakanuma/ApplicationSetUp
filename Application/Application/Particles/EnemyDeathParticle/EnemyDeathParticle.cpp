1986cb12b82646ef8a95dc92d95860233d0a7d53
nakanuma <k023g0121@g.neec.ac.jp>
2025-05-22 21:30:58 +0900

    パーティクルが複数出るように修正

--- Application/Application/Particles/EnemyDeathParticle/EnemyDeathParticle.cpp
+++ Application/Application/Particles/EnemyDeathParticle/EnemyDeathParticle.cpp
@@ -17,9 +17,13 @@ EnemyDeathParticleData EnemyDeathParticle::CreateParticle(const Float3& pos)
 
 	EnemyDeathParticleData p;
 	p.transform.translate = pos + Float3(rand(rng), rand(rng), rand(rng));
-	p.velocity = {0.0f, 1.0f, 0.0f};
-	p.transform.scale = {1.0f, 1.0f, 1.0f};
-	p.color = {1.0f, 0.5f, 0.2f, 1.0f};
+	p.transform.rotate = { 0.0f, 0.0f, 0.0f };
+	p.transform.scale = { 1.0f, 1.0f, 1.0f };
+	p.velocity = { 0.0f, 1.0f, 0.0f };
+	p.rotationSpeed = { 0.0f, 0.0f, 0.0f };
+	p.color = { 1.0f, 0.5f, 0.2f, 1.0f };
+	p.lifeTime = 2.0f;
+	p.currentTime = 0.0f;
 	return p;
 }
 
@@ -29,4 +33,6 @@ void EnemyDeathParticle::UpdateParticle(EnemyDeathParticleData& p, float dt)
 	float lifeRatio = 1.0f - (p.currentTime / p.lifeTime);
 	p.transform.scale = {lifeRatio, lifeRatio, lifeRatio};
 	p.color = {1.0f, lifeRatio, 0.0f, 1.0f};
+	p.transform.rotate.z += 0.01f;
+	p.transform.rotate.x += 0.01f;
 }
--- Application/Application/Particles/EnemyDeathParticle/EnemyDeathParticle.h
+++ Application/Application/Particles/EnemyDeathParticle/EnemyDeathParticle.h
@@ -11,8 +11,10 @@ struct EnemyDeathParticleData
 	Float3 velocity;
 	Float3 rotationSpeed;
 	Float4 color;
-	float lifeTime = 2.0f;
-	float currentTime = 0.0f;
+	float lifeTime;
+	float currentTime;
+
+	// その他パラメーター追加
 };
 
 class EnemyDeathParticle : public BaseParticleEffect<EnemyDeathParticleData>
@@ -21,7 +23,13 @@ public:
 	EnemyDeathParticle(ModelManager::ModelData& model);
 
 protected:
+	/// <summary>
+	/// パーティクル固有の生成処理
+	/// </summary>
 	EnemyDeathParticleData CreateParticle(const Float3& pos) override;
 
+	/// <summary>
+	/// パーティクル固有の更新処理
+	/// </summary>
 	void UpdateParticle(EnemyDeathParticleData& p, float dt) override;
 };
